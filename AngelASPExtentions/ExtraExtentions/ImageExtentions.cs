be73b3b41933401a6792777033eb6155b00c6055
Angelo Sanches <asanches2@studentmail.nait.ca>
2017-09-13 13:30:12 -0600

    Added some commenting i forgot to do

--- AngelASPExtentions/ExtraExtentions/ImageExtentions.cs
+++ AngelASPExtentions/ExtraExtentions/ImageExtentions.cs
@@ -11,6 +11,11 @@ namespace AngelASPExtentions.ExtraExtentions
 {
     public static class ImageExtentions
     {
+        /// <summary>
+        /// Converts a Bitmap to a byte array for serialization
+        /// </summary>
+        /// <param name="This">The bitmap to act on</param>
+        /// <returns>serializated byte array</returns>
         public static byte[] ToByteArray(this Bitmap This)
         {
             var ms = new MemoryStream();
@@ -18,7 +23,11 @@ namespace AngelASPExtentions.ExtraExtentions
             return ms.ToArray();
         }
 
-        //Convert byte[] array to Image:
+        /// <summary>
+        /// Converts a byte array to a Bitmap for deserialization as a image
+        /// </summary>
+        /// <param name="This">The byte array to act on</param>
+        /// <returns>bitmap for an image</returns>
         public static Bitmap ToImage(this byte[] This)
         {
             Bitmap Return = null;
@@ -27,11 +36,21 @@ namespace AngelASPExtentions.ExtraExtentions
             return Return;
         }
 
+        /// <summary>
+        /// Converts a byte array to a base 64 string for deserialization as a html/xml image data
+        /// </summary>
+        /// <param name="This">The byte array to act on</param>
+        /// <returns>base 64 string for an image</returns>
         public static string ToBase64ImageString(this byte[] This)
         {
             return Convert.ToBase64String(This);
         }
 
+        /// <summary>
+        /// Converts a bitmap to a base 64 string for deserialization as a html/xml image data
+        /// </summary>
+        /// <param name="This">The bitmap to act on</param>
+        /// <returns>base 64 string for an image</returns>
         public static string ToBase64ImageString(this Bitmap This)
         {
             return Convert.ToBase64String(This.ToByteArray());
